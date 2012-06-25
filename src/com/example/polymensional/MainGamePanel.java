f6bfec62f1794242772812415cf6207f4048dfd5
Zach Ewert <zachanima@gmail.com>
2012-06-25 22:33:46 +0200

    Add draggable ship class

--- src/com/example/polymensional/MainGamePanel.java
+++ src/com/example/polymensional/MainGamePanel.java
@@ -4,18 +4,25 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import com.example.polymensional.model.Ship;
+
 public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback {
   private MainThread thread;
+  private Ship ship;
   
   public MainGamePanel(Context context) {
     super(context);
     
     // Add callback to surface holder to intercept events.
     getHolder().addCallback(this);
+    
+    // Create ship and load bitmap
+    ship = new Ship(BitmapFactory.decodeResource(getResources(), R.drawable.ship), 50, 50);
     
     // Create the game loop thread.
     thread = new MainThread(getHolder(), this);
@@ -47,16 +54,32 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
   @Override
   public boolean onTouchEvent(MotionEvent event) {
     if (event.getAction() == MotionEvent.ACTION_DOWN) {
+      // Delegate event handling to ship.
+      ship.handleActionDown((int)event.getX(), (int)event.getY());
+      
+      // Exit if 50 bottom pixels are touched.
       if (event.getY() > getHeight() - 50) {
         thread.setRunning(false);
         ((Activity)getContext()).finish();
       }
     }
-    return super.onTouchEvent(event);
+    if (event.getAction() == MotionEvent.ACTION_MOVE) {
+      if (ship.isTouched()) {
+        ship.setX((int)event.getX());
+        ship.setY((int)event.getY());
+      }
+    }
+    if (event.getAction() == MotionEvent.ACTION_MOVE) {
+      if (ship.isTouched()) {
+        ship.setTouched(false);
+      }
+    }
+    return true;
   }
   
   @Override
   protected void onDraw(Canvas canvas) {
-    canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ship), 10, 10, null);
+    canvas.drawColor(Color.BLACK);
+    ship.draw(canvas);
   }
 }
